4272c81d28c82e157a64c5b41f6cce8aa10938f3
pseres <peet.seres@gmail.com>
2021-03-22 19:49:43 +0100

    added reading in all transcripts. issue with content appearing before person tag.

--- read_transcript.py
+++ read_transcript.py
@@ -1,47 +1,62 @@
 from file_utils import load_json
 from dataclasses import dataclass
-from typing import List, Tuple
+from typing import List, Iterator
+
+
+players = ["TRAVIS", "LAURA", "BRIAN", "SAM", "LIAM", "ASHLEY", "TALIESIN", "MARISHA"]
+dm = "MATT"
 
 
 @dataclass
 class TextPiece:
+    """ Data model for a singular piece of dialogue. """
     who: str
     text: str
 
 
-players = ["TRAVIS, LAURA, BRIAN, SAM, ASHLEY, TALESIN, MARISHA"]
-dm = "MATT"
+def generate_text_pieces(episode_data: List[dict]) -> Iterator[TextPiece]:
+    """ Generates all text pieces for a single episode. """
 
+    person_hold = ""
+    accumulated_string = ""
 
-class Episode:
-    def __init__(self, episode_data: List[dict]):
-        self.texts = []
+    for t in episode_data:
 
-        # Iterate through the text elements and parse out who's talking.
-        # The person talking is only specified when it changes, so there is a string buffer,
+        text = t["text"]
+        text = text.replace("\n", " ")
 
-        person_hold = ""
-        accumulated_string = ""
+        # todo: sometimes there is content before the name!!!
 
-        for t in episode_data:
+        split = [s.strip() for s in text.split(":")]
 
-            text = t["text"]
-            text = text.replace("\n", " ")
-            split = [s.strip() for s in text.split(":")]
+        if len(split) == 1:
+            accumulated_string += " " + split[0]
 
-            if len(split) == 1:
-                accumulated_string += " " + split[0]
+        elif len(split) == 2:
+            person, string = split
 
-            elif len(split) == 2:
-                person, string = split
+            text_piece = TextPiece(who=person_hold, text=accumulated_string)
 
-                text_piece = TextPiece(who=person_hold, text=accumulated_string)
-                self.texts.append(text_piece)
+            person_hold = person
+            accumulated_string = string
 
-                person_hold = person
-                accumulated_string = string
+            yield text_piece
 
 
 if __name__ == '__main__':
+    # All of it:
     data = load_json("data/transcripts_c2.json")
-    ep1 = Episode(episode_data=data["0"])
+
+    # Just episode 1:
+    ep1_data = data["0"]
+
+    # Generate the text pieces:
+    texts_ep1 = list(generate_text_pieces(episode_data=ep1_data))
+
+    # Let's do all of it:
+    texts = []
+    for idx, episode_data in data.items():
+        texts += list(generate_text_pieces(episode_data=episode_data))
+
+    matt_texts = [t for t in texts if t.who == "MATT"]
+    people = set([t.who for t in texts])
