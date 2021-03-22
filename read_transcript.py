f4c515dbf97ad8ad8abc92bd98ba8f73766b2a36
pseres <peet.seres@gmail.com>
2021-03-22 20:45:20 +0100

    Added regex parsing of who's talking.

--- read_transcript.py
+++ read_transcript.py
@@ -1,9 +1,10 @@
 from file_utils import load_json
 from dataclasses import dataclass
 from typing import List, Iterator
+import re
 
-
-players = ["TRAVIS", "LAURA", "BRIAN", "SAM", "LIAM", "ASHLEY", "TALIESIN", "MARISHA"]
+players = ["TRAVIS", "LAURA", "BRIAN", "SAM", "LIAM", "ASHLEY", "TALIESIN", "MARISHA", "ALL", "ASHLY", "MARK", "DANI",
+           "AUDIENCE"]
 dm = "MATT"
 
 
@@ -25,20 +26,29 @@ def generate_text_pieces(episode_data: List[dict]) -> Iterator[TextPiece]:
         text = t["text"]
         text = text.replace("\n", " ")
 
-        # todo: sometimes there is content before the name!!!
+        pattern = r'([A-Z]{3,}):'
 
-        split = [s.strip() for s in text.split(":")]
+        split = re.split(pattern, text)
 
         if len(split) == 1:
-            accumulated_string += " " + split[0]
+            accumulated_string += split[0]
 
-        elif len(split) == 2:
-            person, string = split
+        if len(split) == 2:
+            print("This shouldn't happen")
+            raise Exception(f"Unhandled case. For string {text} with split: {split}")
 
-            text_piece = TextPiece(who=person_hold, text=accumulated_string)
+        if len(split) == 3:
+            before, person, after = split
+
+            accumulated_string += " " + before
+
+            if person == '':
+                print(f"another edge case - null person: {person}, {text}, {split}")
+
+            text_piece = TextPiece(who=person_hold, text=accumulated_string.strip())
 
             person_hold = person
-            accumulated_string = string
+            accumulated_string = after
 
             yield text_piece
 
@@ -55,8 +65,24 @@ if __name__ == '__main__':
 
     # Let's do all of it:
     texts = []
-    for idx, episode_data in data.items():
-        texts += list(generate_text_pieces(episode_data=episode_data))
+    for idx, ep_data in data.items():
+        texts += list(generate_text_pieces(episode_data=ep_data))
 
     matt_texts = [t for t in texts if t.who == "MATT"]
     people = set([t.who for t in texts])
+
+    m = parsing_mistakes = [t for t in texts if t.who not in players and t.who != "MATT"]
+
+
+def test_regex_pattern():
+    import re
+    pattern = r'([A-Z]{3,}):'
+
+    x = "just a normal string"
+    y = "MATT: This starts a new one"
+    z = "this has text before, MATT: wooah"
+
+    print(re.split(pattern, x))
+    print(re.split(pattern, y))
+    print(re.split(pattern, z))
+
